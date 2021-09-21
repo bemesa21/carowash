d5a048a14213cd2bbba3385d3612a5ac100a6297
Berenice Medel <bemesa21@gmail.com>
2021-09-21 00:14:02 -0500

    submit profile photo

---
+++ carowash/api/StorageService.swift
@@ -0,0 +1,50 @@
+//
+//  StorageService.swift
+//  carowash
+//
+//  Created by Berenice Medel on 20/09/21.
+//
+
+import Foundation
+import FirebaseStorage
+import FirebaseAuth
+import FirebaseDatabase
+
+class StorageService {
+    static func savePhoto(uid: String, data: Data, metadata: StorageMetadata, onSuccess: @escaping() -> Void, onError: @escaping(_ errorMessage: String) -> Void){
+        let storageRef = Ref().storageSpecificProfile(uid: uid)
+        storageRef.putData(data, metadata: metadata) { (storageMetadata, error) in
+            if error != nil {
+                onError(error!.localizedDescription)
+                return
+            }
+            storageRef.downloadURL { (url, error) in
+                if error != nil {
+                    onError(error!.localizedDescription)
+                    return
+                }
+                if let metaImageUrl = url?.absoluteString{
+                    if let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest(){
+                        changeRequest.photoURL = url
+                        changeRequest.commitChanges { (error) in
+                            if let error = error{
+                                onError(error.localizedDescription)
+                                return
+                            }
+                        }
+                    }
+                    let userInfo: [String: String] = [
+                        "profileImageUrl": metaImageUrl
+                    ]
+                    Ref().databaseSpecificUser(uid: uid).updateChildValues(userInfo) { (error, reference) in
+                        if error == nil {
+                            onSuccess()
+                        }else{
+                            onError(error!.localizedDescription)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
