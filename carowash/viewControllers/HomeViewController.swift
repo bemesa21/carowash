a28bf88be32a3a95e1b862d488aded59d94a7458
Omar Orlando <monkeydoro@gmail.com>
2021-09-25 05:13:37 -0500

    Finished Map View
     -> Is missing: saved locations
    
     *Added label tag to HomeVC
     *LocationButton and functionality
     *Fixed overlaying errors on view

--- carowash/viewControllers/HomeViewController.swift
+++ carowash/viewControllers/HomeViewController.swift
@@ -6,38 +6,99 @@
 //
 
 import UIKit
+import MapKit
+
+private let reuseIdentifier = "ComboCell"
 
 class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - Properties
+    private let tableView = UITableView()
 
-        view.backgroundColor = .lightGray
-        view.addSubview(mapButton)
-        setUpMapButton()
-    }
+    var addressLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.textColor = UIColor.black
+        return label
+    }()
 
     let mapButton: UIButton = {
+        let mapImage = UIImage(systemName: "mappin.circle.fill")
+        mapImage?.withTintColor(UIColor.black)
+
         let button = UIButton(type: .system)
-        button.backgroundColor = UIColor.CarOWash.cerise
-        button.setTitle("Map View", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 5
-        button.setTitleColor(UIColor.black, for: .normal)
+        button.setImage(mapImage, for: .normal)
+        button.colorful()
         button.addTarget(self, action: #selector(showNextView), for: .touchUpInside)
 
         return button
     }()
 
-    func setUpMapButton() {
-        mapButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        mapButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        mapButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
+    // MARK: - Configurations
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
     }
+
+    func configureUI() {
+        view.backgroundColor = UIColor.CarOWash.babyBlue
+        setUpMapButton()
+    }
+
+    func setUpMapButton() {
+        view.addSubview(mapButton)
+        mapButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
+                         paddingTop: 16, paddingLeft: 20, width: 50, height: 30)
+
+        view.addSubview(addressLabel)
+        addressLabel.centerY(inView: mapButton)
+        addressLabel.centerX(inView: view)
+        addressLabel.anchor(left: mapButton.rightAnchor, paddingLeft: 10)
+    }
+
+    func setUpTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.register(ComboCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.tableFooterView = UIView()
+
+//        let height = view.frame.height - locationInputViewHeight
+//        tableView.frame = CGRect(x: 0, y: view.frame.height,
+//                                 width: view.frame.width, height: height)
+
+        view.addSubview(tableView)
+    }
+
+    // MARK: - Selectors
 
     @objc func showNextView() {
         let mlvc = MyLocationViewController()
         mlvc.modalPresentationStyle = .fullScreen
         present(mlvc, animated: true, completion: nil)
+    }
+}
+
+// MARK: - TableView[Delegate/DataSource]
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? ComboCell
+        return cell!
+    }
+}
+
+// MARK: - TabBarDelegate
+
+extension HomeViewController: TabBarDelegate {
+    func displayAddressLabel(dir: String) {
+        addressLabel.text = dir
     }
 }
